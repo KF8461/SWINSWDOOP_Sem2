d47a0818553149bd5f4b3bce3f5a962b4f4f5300
K. F. <KF8461@hotmail.com>
2018-08-06 11:15:07 +1000

    Initial Commit

---
+++ Ahn Needs to test his stuff/ConsoleApplication2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal Fib1 = 1;
+            decimal Fib2 = 1;
+            decimal Fib3 = 0;
+            int counter = 3;
+            decimal Fact = 0;
+            bool exit = false;
+            do
+            {
+                Fact = 1;
+                for (int i = 1; i<= counter; i++)
+                {
+                    Fact = Fact * i;
+                }
+                Console.WriteLine(Fact);
+                do
+                {
+                    Fib3 = Fib1 + Fib2;
+                    Fib1 = Fib2;
+                    Fib2 = Fib3;
+                } while (Fib3 < Fact);
+                if (Fib3 == Fact)
+                {
+                    exit = true;
+                }
+                Console.WriteLine(counter);
+                Console.WriteLine(Fact);
+                counter++;
+            } while (exit==false);
+            Console.WriteLine("Found It");
+            Console.WriteLine(Fib3);
+            Console.WriteLine(counter);
+            Console.WriteLine(Fact);
+            Console.ReadLine();
+        }  
+    }
+}
---
+++ Prime Fibonacci/ConsoleApplication1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> Fibonacci = new List<int>();
+            List<int> PrimeFib = new List<int>();
+            int Fib1 = 1;
+            int Fib2 = 1;
+            int Fib3 = 0;
+            int Counter = 0;
+            bool CheckPrime = false;
+            bool exit = false;
+            Fibonacci.Add(Fib2);
+            do
+            {
+                Fib3 = Fib1 + Fib2;
+                Console.WriteLine(Fib3);
+                Fibonacci.Add(Fib3);
+                for (int i = (Fib3 - 1); i > 1; i--)
+                {
+                    if ((Fib3 % i) == 0)
+                    {
+                        CheckPrime = true;
+                    }
+                }
+                if (CheckPrime == false)
+                {
+                    Counter++;
+                    PrimeFib.Add(Fib3);
+                }
+                if (Counter == 8)
+                {
+                    exit = true;
+                }
+                Fib1 = Fib2;
+                Fib2 = Fib3;
+                CheckPrime = false;
+            } while (exit == false);
+            foreach (int s in PrimeFib)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine();
+            Console.WriteLine(PrimeFib.Sum());
+            Console.ReadLine();
+        }
+    }
+}
